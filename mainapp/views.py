7ccd2ccec38865b3185fc5e49bce4072b0c022c1
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-01-12 16:22:57 +0100

    task 4

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from datetime import datetime
 
 
 class MainPageView(TemplateView):
@@ -15,6 +16,7 @@ class NewsPageView(TemplateView):
         context["news_title"] = "Громкий новостной заголовок"
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
         return context
 
 
