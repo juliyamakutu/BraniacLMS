22c574a1b253a9b2f7b1a7fd1b7a148da4dd451e
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-01-17 17:06:55 +0100

    lesson_4 Display model's data

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,10 +6,16 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-	path("", views.MainPageView.as_view()),
-	path("news/", views.NewsPageView.as_view()),
-	path("courses/", views.CoursesPageView.as_view()),
-	path("contacts/", views.ContactsPageView.as_view()),
-	path("doc_site/", views.DocSitePageView.as_view()),
-	path("login/", views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
