4eeec7279b4667dad54383bc44ccd62d48b0bbe1
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-01-23 18:03:07 +0100

    lesson_6 Feedbacks with AJAX

---
+++ mainapp/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from mainapp import models as mainapp_models
+
+
+class CourseFeedbackForm(forms.ModelForm):
+    def __init__(self, *args, course=None, user=None, **kwargs):
+        ret = super().__init__(*args, **kwargs)
+        if course and user:
+            self.fields["course"].initial = course.pk
+            self.fields["user"].initial = user.pk
+        return ret
+
+    class Meta:
+        model = mainapp_models.CourseFeedback
+        fields = ("course", "user", "feedback", "rating")
+        widgets = {
+            "course": forms.HiddenInput(),
+            "user": forms.HiddenInput(),
+            "rating": forms.RadioSelect(),
+        }
+
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -49,6 +50,19 @@ class Courses(models.Model):
         self.save()
 
 
+class CourseFeedback(models.Model):
+    RATING = ((5, "⭐⭐⭐⭐⭐"), (4, "⭐⭐⭐⭐"), (3, "⭐⭐⭐"), (2, "⭐⭐"), (1, "⭐"))
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, verbose_name=_("Course"))
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("User"))
+    feedback = models.TextField(default=_("No feedback"), verbose_name=_("Feedback"))
+    rating = models.SmallIntegerField(choices=RATING, default=5, verbose_name=_("Rating"))
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.course} ({self.user})"
+
+
 class Lesson(models.Model):
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     num = models.PositiveIntegerField(verbose_name="Lesson number")
@@ -89,4 +103,3 @@ class CourseTeachers(models.Model):
     class Meta:
         verbose_name = _("Teacher")
         verbose_name_plural = _("Teachers")
-        
