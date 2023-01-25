f530aa00952442b355fa004d384c85e5362d9c5f
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-01-25 14:24:27 +0100

    lesson_7 celery config hotfix

--- config/settings.py
+++ config/settings.py
@@ -246,8 +246,8 @@ CACHES = {
 }
 
 
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
 
 
 # Read about sending email:
