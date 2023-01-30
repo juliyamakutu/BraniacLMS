bbdd6ac4cbe3eab9836b74a21c7c4a18f2347c39
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-01-30 16:52:09 +0100

    add tests

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -1,3 +1,137 @@
 from django.test import TestCase
 
 # Create your tests here.
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from authapp import models as authapp_models
+from mainapp import models as mainapp_models
+
+
+class TestNewsPage(TestCase):
+    fixtures = (
+        "authapp/fixtures/001_user_admin.json",
+        "mainapp/fixtures/001_news.json",
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.client_with_auth = Client()
+        self.user_admin = authapp_models.CustomUser.objects.get(username="admin")
+        self.client_with_auth.force_login(self.user_admin, backend="django.contrib.auth.backends.ModelBackend")
+
+    def test_page_open_list(self):
+        path = reverse("mainapp:news")
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+    def test_page_open_detail(self):
+        news_obj = mainapp_models.News.objects.first()
+        path = reverse("mainapp:news_detail", args=[news_obj.pk])
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+    def test_page_open_crete_deny_access(self):
+        path = reverse("mainapp:news_create")
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+    def test_page_open_crete_by_admin(self):
+        path = reverse("mainapp:news_create")
+        result = self.client_with_auth.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+    def test_create_in_web(self):
+        counter_before = mainapp_models.News.objects.count()
+        path = reverse("mainapp:news_create")
+        self.client_with_auth.post(
+            path,
+            data={
+                "title": "NewTestNews001",
+                "preambule": "NewTestNews001",
+                "body": "NewTestNews001",
+            },
+        )
+        self.assertGreater(mainapp_models.News.objects.count(), counter_before)
+
+    def test_page_open_update_deny_access(self):
+        news_obj = mainapp_models.News.objects.first()
+        path = reverse("mainapp:news_update", args=[news_obj.pk])
+        result = self.client.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+    def test_page_open_update_by_admin(self):
+        news_obj = mainapp_models.News.objects.first()
+        path = reverse("mainapp:news_update", args=[news_obj.pk])
+        result = self.client_with_auth.get(path)
+        self.assertEqual(result.status_code, HTTPStatus.OK)
+
+    def test_update_in_web(self):
+        new_title = "NewTestTitle001"
+        news_obj = mainapp_models.News.objects.first()
+        self.assertNotEqual(news_obj.title, new_title)
+        path = reverse("mainapp:news_update", args=[news_obj.pk])
+        result = self.client_with_auth.post(
+            path,
+            data={
+                "title": new_title,
+                "preambule": news_obj.preambule,
+                "body": news_obj.body,
+            },
+        )
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+        news_obj.refresh_from_db()
+        self.assertEqual(news_obj.title, new_title)
+
+    def test_delete_deny_access(self):
+        news_obj = mainapp_models.News.objects.first()
+        path = reverse("mainapp:news_delete", args=[news_obj.pk])
+        result = self.client.post(path)
+        self.assertEqual(result.status_code, HTTPStatus.FOUND)
+
+    def test_delete_in_web(self):
+        news_obj = mainapp_models.News.objects.first()
+        path = reverse("mainapp:news_delete", args=[news_obj.pk])
+        self.client_with_auth.post(path)
+        news_obj.refresh_from_db()
+        self.assertTrue(news_obj.deleted)
+
+import pickle
+from unittest import mock
+
+
+class TestCoursesWithMock(TestCase):
+    fixtures = (
+        "authapp/fixtures/001_user_admin.json",
+        "mainapp/fixtures/002_courses.json",
+        "mainapp/fixtures/003_lessons.json",
+        "mainapp/fixtures/004_teachers.json",
+    )
+
+    def test_page_open_detail(self):
+        course_obj = mainapp_models.Courses.objects.get(pk=1)
+        path = reverse("mainapp:courses_detail", args=[course_obj.pk])
+        with open("mainapp/fixtures/005_feedback_list_1.bin", "rb") as inpf, mock.patch(
+            "django.core.cache.cache.get"
+        ) as mocked_cache:
+            mocked_cache.return_value = pickle.load(inpf)
+            result = self.client.get(path)
+            self.assertEqual(result.status_code, HTTPStatus.OK)
+            self.assertTrue(mocked_cache.called)
+
+from django.core import mail as django_mail
+
+from mainapp import tasks as mainapp_tasks
+
+
+class TestTaskMailSend(TestCase):
+    fixtures = ("authapp/fixtures/001_user_admin.json",)
+
+    def test_mail_send(self):
+        message_text = "test_message_text"
+        user_obj = authapp_models.CustomUser.objects.first()
+        mainapp_tasks.send_feedback_mail({"user_id": user_obj.id, "message": message_text})
+        self.assertEqual(django_mail.outbox[0].body, message_text)
+        
